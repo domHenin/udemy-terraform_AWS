3347631b2fce4f08bcbc8bcd88b7a4a5478b1a3c
domHenin <domhenin.517@gmail.com>
2021-01-18 12:39:38 -0500

    completed challenge, functional code

--- section06_ec2-challenge/variables.tf
+++ section06_ec2-challenge/variables.tf
@@ -1,4 +1,16 @@
 # Variable: AWS Region
 variable "aws_region" {
-    default = "us-east-1"
+  default = "us-east-1"
+}
+
+# Variable: Ingress Rules
+variable "ingressrules" {
+  type    = list(number)
+  default = [80, 443]
+}
+
+# Variable: Egress Rules
+variable "egressrules" {
+  type    = list(number)
+  default = [80, 443]
 }
