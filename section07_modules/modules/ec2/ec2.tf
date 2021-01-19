7415b2c60227af3506b0ffbb3a18543dee4a2deb
domHenin <domhenin.517@gmail.com>
2021-01-19 14:34:32 -0500

    completed tutorial

--- section07_modules/modules/ec2/ec2.tf
+++ section07_modules/modules/ec2/ec2.tf
@@ -1,9 +1,13 @@
 # Resource: EC2 Instance
 resource "aws_instance" "ec2" {
-    ami = "ami-032598fcc7e9d1c7a"
-    instance_type = "t2.micro"
+  ami           = "ami-032598fcc7e9d1c7a"
+  instance_type = "t2.micro"
 
-    tags = {
-        Name = var.ec2_name
-    }
+  tags = {
+    Name = var.ec2_name
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.ec2.id
 }
