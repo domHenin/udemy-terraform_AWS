f48d64d46384ab6f929135569645eed5f771ced2
domHenin <domhenin.517@gmail.com>
2021-03-07 01:28:06 -0500

    created directory

---
+++ section00_module-challenge/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = var.aws_region
+}
+
+
+// TODO:
+// 1. create a DB Server (ec2 instance with this title) and output the private IP
+// 2. Create a web server and ensure it has a fixed public IP
+// 3. Create a Security Group for the web server opening ports 80 and 443 (HTTP & HTTPS)
+// 4. Run the provided script on the web server
